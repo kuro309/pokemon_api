efeac781280e9bdc0924e28be185bb69c389e33f
kuro309 <kr30931911@outlook.jp>
2024-08-28 22:57:56 +0900

    ポケモンの名前を日本語で表示する

--- src/app/components/FechData.js
+++ src/app/components/FechData.js
@@ -3,18 +3,30 @@ import React, { useState, useEffect }  from "react";
 
 const FetchData = () => {
   const [pokemon, setPokemon] = useState(undefined);
+  const [pokemon_zukan, setPokemonZukan] = useState(undefined);
+  let index = 0;
+  let id = 151;
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/3/")
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
       .then((res) => res.json())
       .then((json) => setPokemon(json))
       .catch(() => alert("error"));
  
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
+      .then((res) => res.json())
+      .then((json) => {
+        setPokemonZukan(json)
+        index = json["names"].findIndex((item) => item.language.name === "ja-Hrkt");
+      })
+      .catch(() => alert("error"));
+      
   }, []);
-   
-  console.log(pokemon);
+  // console.log(pokemon_zukan); 
+  // console.log(index);
+  // console.log(pokemon_zukan["names"][index]["name"]);
   return <>
-    {pokemon === undefined ? "" : <div>{pokemon["name"]}</div>}
+    {pokemon_zukan === undefined ? "" : <div>{pokemon_zukan["names"][index]["name"]}</div>}
     {pokemon === undefined ? "" : <img src={pokemon["sprites"]["front_default"]} />}
    </>;
 };
